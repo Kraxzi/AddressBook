cb256fba36fcc5d3a1b37bd786e3407cccf18d39
Kraxzi <klabunskyi@gmail.com>
2023-08-16 00:00:50 +0300

    fixed connection

--- src/db/pg.connection.ts
+++ src/db/pg.connection.ts
@@ -5,6 +5,7 @@ export const postgresDB = knex({
   client: "pg",
   connection: {
     host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     port: Number(process.env.POSTGRES_PORT),
